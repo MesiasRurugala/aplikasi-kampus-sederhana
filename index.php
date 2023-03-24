a217182b1e8f9cad9d4cd704bb1a3c11b5299551
MesiasRurugala <128768916+MesiasRurugala@users.noreply.github.com>
2023-03-24 11:48:33 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html  lang="en">
+
+<head>   
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <title>halaman utama</title>
+</head>
+<body>
+    <h3>ini adalah judul</h3>
+    <p>ini adalah artikel</p>
+</body>
+</html> 
